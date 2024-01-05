5eaece94aa76dd117f390d8d3280f92081cbc70d
fenilfaldu1 <155763749+fenilfaldu1@users.noreply.github.com>
2024-01-05 14:17:21 -0500

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
-print("Third program in github hello fenil faldu 1")
+print("Third program in github hello fenil faldu 3")
 =======
-print("Second program in github hello fenil faldu 1")
+print("Second program in github hello fenil faldu 3")
 >>>>>>> 902d5c57a2aa2a3db8cf88f050a78f0eda901f60
